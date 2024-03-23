d39d87025a098990584d5c148c70298c1ae8f05c
Albert Julian Tannady <albertjulian97@gmail.com>
2024-03-24 01:39:37 +0800

    Add Intro Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/github',
+                destination: 'https://github.com/bilbatez',
+                permanent: false
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/albertjt/',
+                permanent: false
+            }
+        ]
+    }
+};
 
 export default nextConfig;
