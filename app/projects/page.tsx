a9c8b6a653ddd8263e94b7f0a6aee25731e204d0
Albert Julian Tannady <albertjulian97@gmail.com>
2024-05-22 20:52:48 +0700

    Refactor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,39 @@
+import Link from "next/link"
+
 export default function Projects() {
+    const projects = [
+        {
+            title: "KPR for Dummies",
+            link: "",
+            description: "Home mortgage calculator based on Indonesia.",
+        },
+        {
+            title: "Impractical Python Projects",
+            link: "",
+            description: "A collection of small python scripts.",
+        },
+        {
+            title: "Miniature Javascripts Projects",
+            link: "",
+            description: "A collection of small javascript projects.",
+        }
+    ]
+
+    function ProjectItemComponents() {
+        return projects.map(project => {
+            return (
+                <li key={project.title}>
+                    <b><Link href={project.link}>[{project.title}]</Link></b>: {project.description}
+                </li>
+            )
+        })
+    }
+
     return (
         <>
             <p>Here are the tech projects that I&apos;ve worked on!</p>
             <ul className="mt-4 list-disc list-inside">
-                <li></li>
+                <ProjectItemComponents/>
             </ul>
         </>
     )
