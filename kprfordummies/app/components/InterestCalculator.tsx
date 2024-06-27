e167ccc71297ab123789440d6336e028fb5ec62f
Albert Julian Tannady <albertjulian97@gmail.com>
2024-06-28 01:28:30 +0800

    Add dynamic interest period field

--- kprfordummies/app/components/InterestCalculator.tsx
+++ kprfordummies/app/components/InterestCalculator.tsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 import clsx from "clsx/lite";
 import NumberInputField from "./NumberInputField";
 import DateInputField from "./DateInputField";
+import InterestPeriodField from "./InterestPeriodField";
 
 function InterestCalculator({
     className,
@@ -14,13 +15,10 @@ function InterestCalculator({
         <div className="max-w-lg mx-auto">
             <form className={clsx(hidden && "hidden", className)}>
                 <NumberInputField id="principal" label="Pokok Pinjaman (Rp.)" placeholder="100000000" />
-                <div className="flex">
-                    <NumberInputField id="interest" label="Suku Bunga (%)" min={0} max={20} width="w-1/2" placeholder="5" />
-                    <NumberInputField id="period" label="Lama Pinjaman (Dalam Tahun)" min={1} max={30} width="w-1/2" placeholder="15" containerClassName="ml-1" />
-                </div>
+                <InterestPeriodField />
                 <DateInputField id="startDate" label="Tanggal Mulai" placeholder="30/07/2015" />
                 <div className="flex">
-                    <button type="reset" className="w-1/2">Ulangi</button>
+                    <button type="reset" className="w-1/2 btn-sd">Ulangi</button>
                     <button type="submit" className="w-1/2 btn-sb">Kalkulasi</button>
                 </div>
             </form>
---
+++ kprfordummies/app/components/InterestPeriodField.tsx
@@ -0,0 +1,75 @@
+import { memo, useContext, useState } from "react";
+import NumberInputField from "./NumberInputField";
+import { CurrentInterestType } from "@/types/context";
+import { CurrentInterestTypeContext } from "../context";
+import clsx from "clsx/lite";
+import { InterestType } from "@/types/interest";
+
+function InterestPeriodField() {
+
+    const MIN_FIELD = 1
+    const MAX_FIELD = 5
+
+    const { currentInterestType }: CurrentInterestType = useContext(CurrentInterestTypeContext)
+    const [currentNumberOfField, setCurrentNumberOfField] = useState(MIN_FIELD)
+
+    function hasMultipleInterestPeriod(): boolean {
+        return new Set<InterestType>([
+            InterestType.EFFECTIVE,
+            InterestType.FLOATING
+        ]).has(currentInterestType)
+    }
+
+    function hasDeletableField(): boolean {
+        return currentNumberOfField > 1
+    }
+
+    function isMinimumNumberOfField(): boolean {
+        return currentNumberOfField == MIN_FIELD
+    }
+
+    function isMaximumNumberOfField(): boolean {
+        return currentNumberOfField == MAX_FIELD
+    }
+
+    function handleClickAdd(): void {
+        setCurrentNumberOfField(() => currentNumberOfField + 1)
+    }
+
+    function handleClickDelete(): void {
+        setCurrentNumberOfField(() => currentNumberOfField - 1)
+    }
+
+    function getFields() {
+        const result = new Array(currentNumberOfField)
+        for (let index = 0; index < currentNumberOfField; index++) {
+            result[index] = (
+                <div className="flex" key={"interestperiod_" + index}>
+                    <NumberInputField id="interest" label="Suku Bunga (%)" min={0} max={20} width="w-1/2" placeholder="5" />
+                    <NumberInputField id="period" label="Lama Pinjaman (Dalam Tahun)" min={1} max={30} width="w-1/2" placeholder="15" containerClassName="ml-1" />
+                </div>
+            )
+        }
+        return result
+    }
+
+    return (
+        <>
+            {getFields()}
+            <div className="flex mb-5" hidden={!hasMultipleInterestPeriod()}>
+                <button type="button" onClick={handleClickDelete} className={clsx(
+                    "btn-sd",
+                    isMaximumNumberOfField() ? 'w-full' : 'w-1/2',
+                    !hasDeletableField() && 'hidden'
+                )}>Hapus Suku Bunga</button>
+                <button type="button" onClick={handleClickAdd} className={clsx(
+                    "btn-sd",
+                    isMinimumNumberOfField() ? 'w-full' : 'w-1/2',
+                    (!hasMultipleInterestPeriod() || isMaximumNumberOfField()) && 'hidden'
+                )}>Tambah Suku Bunga</button>
+            </div>
+        </>
+    )
+}
+
+export default memo(InterestPeriodField)
